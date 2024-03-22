aa734bb1b5e6e86c0ada040d2a6b2f49d6d4c79c
Mitchell Kurtz <mitchelljkurtz123@gmail.com>
2024-03-22 13:17:47 -0400

    Update Plot Legend and Labels

--- Test_Kurtz_Zeta_Approximator_Main.m
+++ Test_Kurtz_Zeta_Approximator_Main.m
@@ -6,21 +6,21 @@
 % see display_kurtz_zeta_approximation_error_vs_num_discrete_steps.m;
 % In general,
 % 1.) As real(testInputValues) increases, the accuracy of the approximation increases
-% 2.) As the number of steps used in the approximation increases, the accuracy of the approximation increases*
+% 2.) As the number of steps used in the approximation increases, the accuracy of the approximation increases in a damped harmonic logarithmic fashion
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 addpath('./Tools')
 clc;clear;format long g
 testInputValues= {};
-testInputValues{1,1} = 0.3 + 0.2532i;
-testInputValues{2,1} = 0.4 - 0.2352i;
-testInputValues{3,1} = 0.5 + 14.1347251417346937904572519835625i;
-testInputValues{4,1} = 0.7 + 52.6i;
-testInputValues{5,1} = 0.852352 + 0.0025489i;
-testInputValues{6,1} = 0.95 + 1203i;
-testInputValues{7,1} = 1.4235 + 5.2456i;
-testInputValues{8,1} = 2 + 0i;
-testInputValues{9,1} = 3.2 + 57i;
-testInputValues{10,1} = 4.3 + 24i;
+testInputValues{1,1} = 4.3 + 24234i;
+testInputValues{2,1} = 3.2 + 57i;
+testInputValues{3,1} = 2 + 0i;
+testInputValues{4,1} = 1.4235 + 5.2456i;
+testInputValues{5,1} = 0.95 + 1203i;
+testInputValues{6,1} = 0.852352 + 0.0025489i;
+testInputValues{7,1} = 0.7 + 52.6i;
+testInputValues{8,1} = 0.5 + 14.1347251417346937904572519835625i;
+testInputValues{9,1} = 0.4 - 0.2352i;
+testInputValues{10,1} = 0.3 + 0.2532i;
 
 for ind = 1:size(testInputValues,1)
     inputValue = testInputValues{ind,1};
@@ -28,7 +28,7 @@ for ind = 1:size(testInputValues,1)
     % Calculate and display Zeta Funciton Calculation with matlab 
     matlabZetaVal = zeta(inputValue);
     
-    % Calculate Zeta Function Using Kurtz Approximation (Defaults to 10000000 steps)
+    % Calculate Zeta Function Using Kurtz Approximation (Defaults to 5000000 steps)
     kurtzZetaCalculation = kurtzZetaApproximation(inputValue);
     
     
--- display_kurtz_zeta_approximation_error_vs_num_discrete_steps.m
+++ display_kurtz_zeta_approximation_error_vs_num_discrete_steps.m
@@ -1,8 +1,8 @@
 % This Script plots the accuracy of the Kurtz Zeta Approximation as a function of the number of steps used for the calculation
 addpath('./Tools');clc;clear;format long g;
 
-testVal = 0.7 + 10i; % Change this to the value you want to test
-numStepsVect =1:1:10000; % Change this to the step sizes you want to test over
+testVal = 0.7253 + 10.52i; % Change this to the value you want to test
+numStepsVect =100:1:10000; % Change this to the step sizes you want to test over
 
 realOutputVect =zeros(1,size(numStepsVect,2));
 imagOutputVect =zeros(1,size(numStepsVect,2));
@@ -24,36 +24,37 @@ imagError = truthImag - imagOutputVect;
 
 realTruthVect = truthReal*ones(size(realOutputVect));
 imagTruthVect = truthImag*ones(size(imagOutputVect));
+legendNameStr = sprintf('Zeta(%f + (%f) i)\n', real(testVal), imag(testVal));
 
 figure(2)
 subplot(2,2,1)
 hold off
-plot(numStepsVect,realOutputVect, 'DisplayName','Kurtz Approximation');
+plot(numStepsVect,realOutputVect, 'DisplayName',strcat( "Kurtz  Approximation for " , legendNameStr));
 hold on 
-plot(numStepsVect,realTruthVect, 'DisplayName','Matlab Zeta Function');
-title(strcat("Real Kurtz Calculated Value vs Num Steps"))
+plot(numStepsVect,realTruthVect, 'DisplayName',strcat("Matlab Zeta Function for ", legendNameStr));
+title(strcat("Zeta Real Output Value vs Num Steps"))
 xlabel("Number of Steps")
-ylabel("Kurtz Approximation Real Output")
+ylabel("Zeta Real Output Value")
 legend();
 
 subplot(2,2,2)
 plot(numStepsVect, realError)
 xlabel("Number of Steps")
-ylabel("Real Error")
+ylabel("Real Output Error")
 title("Real Error (Matlab Zeta Function - Kurtz Approximation) ")
 
 subplot(2,2,3)
 hold off
-plot(numStepsVect,imagOutputVect, 'DisplayName','Kurtz Approximation');
-title(strcat("Imag Kurtz Calculated Value vs Num Steps "))
+plot(numStepsVect,imagOutputVect, 'DisplayName',strcat( "Kurtz  Approximation for " , legendNameStr));
+title(strcat("Zeta Imaginary Output Value vs Num Steps "))
 hold on 
-plot(numStepsVect,imagTruthVect, 'DisplayName','Matlab Zeta Function');
+plot(numStepsVect,imagTruthVect, 'DisplayName',strcat("Matlab Zeta Function for ", legendNameStr));
 xlabel("Number of Steps")
-ylabel("Kurtz Approximation Imag Output")
+ylabel("Zeta Imaginary Output Value")
 legend()
 
 subplot(2,2,4)
 plot(numStepsVect, imagError)
 xlabel("Number of Steps")
-ylabel("Imag Error")
-title("Imag Error (Matlab Zeta Calculation - Kurtz Approximation) ")
+ylabel("Imaginary Output Error")
+title("Imaginary Output Error (Matlab Zeta Calculation - Kurtz Approximation) ")
